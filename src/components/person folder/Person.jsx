2b6e05b280cd34b0381c50ffee82e91ac63c2f13
Sina Madadi <sinamadadi.work@gmail.com>
2022-12-21 22:14:00 +0330

    saved

--- src/components/person folder/Person.jsx
+++ src/components/person folder/Person.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const Person = ({ fullName, age, personDelete, personEdit }) => {
+const Person = ({ fullName, personDelete, personEdit }) => {
     return (
-        <div>
-            <h2>{`${fullName}`}</h2>
-            <button onClick={personDelete}>حذف</button>
-            <input type="text" placeholder={fullName} onChange={personEdit} />
+        <div className='card text-white bg-info mb-3 mt-3 w-25 mx-auto'>
+            <div className='card'>
+                <h3>{`${fullName}`}</h3>
+                <button onClick={personDelete}>حذف</button>
+                <input type="text" placeholder={fullName} onChange={personEdit} />
+            </div>
         </div>
     );
 }
